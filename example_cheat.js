d582766bb487df59cc9e3c70b2507b2b54ed6a04
bread <63481332+brread@users.noreply.github.com>
2024-04-22 23:17:08 -0500

    Update example_cheat.js

--- example_cheat.js
+++ example_cheat.js
@@ -36,15 +36,15 @@ new Cheat("https://sub2krunkercentral.com/game_1_4.js", {
             weaponSwap: 11,
             moveLock: 12,
         };
- 
+
         // auto jump (hold space to bhop)
         if (this.pressedKeys.has(" ")) {
             this.controls.keys[this.controls.binds.jump.val] ^= 1;
-            
+
             if (this.controls.keys[this.controls.binds.jump.val])
                 this.controls.didPressed[this.controls.binds.jump.val] = 1;
         }
-    
+
         return input;
     },
 
@@ -52,27 +52,39 @@ new Cheat("https://sub2krunkercentral.com/game_1_4.js", {
         const scale = this.scale || parseFloat(RegExp(/\((.+)\)/).exec(document.getElementById("uiBase").style.transform)[1]);
         const width = innerWidth / scale;
         const height = innerHeight / scale;
-        
-        if (this.renderer?.frustum && this.me.active) {
-            // simple box esp
-          
-            const enemies = this.game.players.list.filter(player => (!player.isYou && (!this.me.team || player.team !== this.me.team)));
+
+        // simple box esp + nametag
+
+        const enemies = this.game.players.list.filter(player => (!player.isYou && (!this.me.team || player.team !== this.me.team)));
+
+        for (const enemy of enemies) {
+            const pos = new this.three.Vector3(enemy.x, enemy.y, enemy.z);
+
+            if (enemy.health <= 0 || !enemy.active) continue;
+            if (!this.containsPoint(pos)) continue;
+
+            const top = this.world2Screen(pos.clone(), width, height, enemy.height);
+            const bottom = this.world2Screen(pos.clone(), width, height);
+
+            const rectHeight = Math.round(bottom.y - top.y);
+            const rectWidth = Math.round(rectHeight * 0.65);
+
+            this.ctx.lineWidth = 1;
+            this.ctx.strokeStyle = "#f00";
+
+            this.ctx.strokeRect(Math.round(top.x - rectWidth / 2), Math.round(top.y), Math.round(rectWidth), Math.round(rectHeight));
+
+            if (enemy.cnBSeen) continue;
+
+            this.ctx.font = "Arial 24px";
+            this.ctx.textAlign = "center";
+            this.ctx.fillStyle = "#fff";
+
+            let nametagText = enemy.alias;
             
-            for (const enemy of enemies) {
-                const pos = new this.three.Vector3(enemy.x, enemy.y, enemy.z);
+            if (enemy.clan) nametagText += ` [${enemy.clan}]`;
 
-                if (enemy.health <= 0 || !enemy.active) continue;
-                if (!this.containsPoint(pos)) continue;
-
-                const top = this.world2Screen(pos.clone(), width, height, enemy.height);
-                const bottom = this.world2Screen(pos.clone(), width, height);
-
-                const rectHeight = Math.round(bottom.y - top.y);
-                const rectWidth = Math.round(rectHeight * 0.6);
-
-                this.ctx.strokeStyle = "#f00";
-                this.ctx.strokeRect(top.x - rectWidth / 2, top.y, rectWidth, rectHeight);
-            }
+            this.ctx.fillText(nametagText, Math.round(top.x), Math.round(top.y) - 5);
         }
     }
 }, {
@@ -80,10 +92,10 @@ new Cheat("https://sub2krunkercentral.com/game_1_4.js", {
         pos.y += offsetY;
 
         pos.project(this.renderer.camera);
-        
+
         pos.x = (pos.x + 1) / 2;
         pos.y = (-pos.y + 1) / 2;
-        
+
         pos.x *= width;
         pos.y *= height;
 
